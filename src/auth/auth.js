d97574a737002c8109aafc9523211548a653a356
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-12-03 22:23:51 +0100

    delete console.log

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -5,7 +5,6 @@ const auth = (req, res, next) => {
 
     jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
         if (err || !decoded.userId) res.status(401).json("Vous devez être authentifié pour effectuer ceci")
-        console.log(decoded.userId)
         req.body.userId = decoded.userId
         next()
     })
