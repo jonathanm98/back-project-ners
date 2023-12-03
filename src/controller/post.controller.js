b9873c41270aefdda3d73c99786ff0ca91429217
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-12-03 22:27:43 +0100

    Added Auth middleware, CREATE UPDATE Post and CREATE Comment function

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -14,20 +14,29 @@ const createPost = (req, res) => {
         res.status(201).json(post);
     }).catch((err) => {
         console.log(err);
-        res.status(500).json("Une erreur s'est produite lors de la création du post.");
+        res.status(500).json("Une erreur s'est produite lors de la création du post");
     });
 };
 
-const updatePost = (req, res) => {
-    if (!req.body.content || !req.body.postId) {
+const updatePost = async (req, res) => {
+    const {postId, content, userId} = req.body
+
+    if (!content || !postId) {
         return res.status(400).json("Vous ne pouvez pas envoyer un post vide")
     }
 
-    Post.update({...req.body.data}, {
-        where : {
-            
-        }
-    })
+    try {
+        const post = await Post.findOne({where: {postId}})
+        if (!post) return res.status(401).json("Vous devez être authentifié pour effectuer ceci")
+        post.content = content
+        res.status(201).json(post)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json("Une erreur s'est produite lors de la mise à jour du post")
+    }
 }
 
-module.exports = {createPost}
+module.exports = {
+    createPost,
+    updatePost
+}
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,5 +3,6 @@ const postController = require("../controller/post.controller")
 const {auth} = require("../auth/auth")
 
 router.post("/create", auth, postController.createPost)
+router.put("/update", auth, postController.updatePost)
 
 module.exports = router
