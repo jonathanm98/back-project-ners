d75d19a3009e3ec7a02e0f61d64efbe10cb1ecd3
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-12-10 14:12:56 +0100

    Added likes/unlike endpoint

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -1,4 +1,4 @@
-const {Comment} = require("../db/sequelize")
+const {Comment, Post} = require("../db/sequelize")
 
 const createComment = (req, res) => {
     const {content, userId, postId} = req.body
@@ -16,4 +16,29 @@ const createComment = (req, res) => {
     });
 }
 
-module.exports = {createComment}
+const likeComment = async (req, res) => {
+    const {userId, commentId} = req.body
+    let message = ""
+    try {
+        const comment = await Comment.findByPk(commentId)
+        let likes = JSON.parse(comment.likes)
+        if (!likes.includes(userId)) {
+            likes.push(userId)
+            message = "Le like a bien été ajouté"
+        } else {
+            likes = likes.filter(id => id !== userId);
+            message = "Le like a bien été supprimé"
+        }
+        comment.likes = JSON.stringify(likes)
+        await comment.save()
+        res.status(201).json({message, data: {...comment.dataValues, likes: JSON.parse(comment.likes)}})
+    } catch(err) {
+        console.log(err)
+        res.status(500).json("Une erreur s'est produite lors de l'ajout du like");
+    }
+}
+
+module.exports = {
+    createComment,
+    likeComment,
+}
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -3,5 +3,6 @@ const commentController = require("../controller/comment.controller")
 const {auth} = require("../auth/auth")
 
 router.post("/create", auth, commentController.createComment)
+router.put("/like", auth, commentController.likeComment)
 
 module.exports = router
